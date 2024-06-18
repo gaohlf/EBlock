6b0b490bc166725e1ec4a7e1e776139147edc77e
gaohlf <20075113ghl@sina.com>
2024-06-18 09:43:21 +0800

    Author:gaohlf Tested-by: gaohlf Test-Method: smoke Description:  删除多余文件

--- EbSys.h
+++
@@ -1,9 +0,0 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/proc_fs.h>
-#include <linux/uaccess.h>
-
-extern void addEBlockSys(void);
-
-extern void rmEBlockSys(void);
